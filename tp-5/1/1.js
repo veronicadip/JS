3e944438e9a2d0bb42ef0cf884bc06ecb86a4c84
Vero <dipmveronica@gmail.com>
2023-05-11 19:05:21 -0300

    agregar tp 5

---
+++ tp-5/1/1.js
@@ -0,0 +1,20 @@
+/* Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número 
+aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número 
+y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo 
+adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
+Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero. */
+
+let numero = 0;
+
+const crearNumero = function () {
+    let min = Math.ceil(1);
+    let max = Math.floor(11);
+    numero = Math.floor(Math.random() * (max - min) + min);
+};
+
+window.onload = function () {
+    const botonJugar = document.getElementById(`botonJugar`);
+    botonJugar.addEventListener(`click`, () => crearNumero());
+    console.log(numero)
+}
+
