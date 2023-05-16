f4930d911c01feb555936461d3e4b0a5033d2119
Vero <dipmveronica@gmail.com>
2023-05-15 21:10:28 -0300

    agregue ejercicio 1

--- tp-5/1/1.js
+++ tp-5/1/1.js
@@ -4,10 +4,38 @@ y un botón enviar, al presionar el botón enviar mostrar en un alert si el usua
 adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero. */
 
+
 let numero = 0;
+let respuesta = 0;
 
 const crearNumero = function () {
     let min = Math.ceil(1);
     let max = Math.floor(11);
     numero = Math.floor(Math.random() * (max - min) + min);
 };
+
+const botonJugar = document.querySelector(".botonJugar");
+const formNumero = document.querySelector(".formNumero");
+const botonRespuesta = document.querySelector(".botonRespuesta");
+
+botonJugar.addEventListener("click", e => {
+    crearNumero();
+    console.log(numero)
+});
+
+formNumero.addEventListener("input", e => {
+    respuesta = parseInt(e.target.value)
+    console.log(respuesta)
+});
+
+botonRespuesta.addEventListener("click", e => {
+    if (numero === respuesta) {
+        alert(`Adivinaste el número mágico!`);
+    } else if (numero < respuesta && respuesta <= 10) {
+        alert(`El número que ingresaste es mayor que el número mágico.`);
+    } else if (numero > respuesta && respuesta >= 1) {
+        alert(`El número que ingresaste es menor que el número mágico.`);
+    } else {
+        alert(`Porfavor, ingresa un número válido`);
+    }
+});
