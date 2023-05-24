6f0cc9b7aaa040ad48a234610c39b1f5bb174de4
veronicadip <dipmveronica@gmail.com>
2023-05-23 23:43:11 -0300

    avances en ejercicio 6

--- tp-5/6/6.js
+++ tp-5/6/6.js
@@ -1,2 +1,25 @@
 /* Realizar una web con un temporizador donde el usuario pueda ingresar un tiempo desde donde comenzará a 
 decrementar el contador. Debe contener los botones, iniciar, pausar y reset. */
+
+let tiempo = 0;
+let convertirMinutos;
+let convertirSegundos;
+
+// convertirSegundos = new Date(numero * 1000).toISOString().slice(11, 19);
+
+const temporizador = document.getElementById("temporizador");
+const iniciar = document.getElementById("iniciar");
+const pausar = document.getElementById("pausar");
+const reset = document.getElementById("reset");
+
+const setearTemporizador = function (numero) {
+  convertirMinutos = new Date(numero * 60000).toISOString().slice(11, 19);
+  temporizador.textContent = convertirMinutos.toString();
+};
+
+const setear = document.getElementById("setear");
+setear.addEventListener("click", (e) => {
+  e.preventDefault();
+  tiempo = e.target.form[0].value;
+  setearTemporizador(tiempo);
+});
